e7037fabc93a5f6b407b8926e2f7cf4db870bfea
Uriel Mori <moriuriel@gmail.com>
2023-03-04 15:42:38 -0300

    chore: ajustando servico prisma

--- src/infrastructure/prisma/prisma.service.ts
+++ src/infrastructure/prisma/prisma.service.ts
@@ -1,10 +1,22 @@
+import { OnModuleDestroy } from '@nestjs/common';
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
