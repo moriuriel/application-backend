c854cbca8236eeffa80503ab74902edc097165c7
Uriel Mori <moriuriel@gmail.com>
2023-03-09 16:35:16 -0300

    refactor: ajustando estrutura de pastas

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,7 @@
 import { PrismaService } from '@Infra/prisma';
 import { Module } from '@nestjs/common';
 import { CreateUserUseCase } from './application/use-cases';
-import { UserController } from './http/controllers/users.controller';
+import { UserController } from './presentations/controllers/users.controller';
 import { UserRepository } from './infrastructure/repositories/user.repository';
 
 @Module({
