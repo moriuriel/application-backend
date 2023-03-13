caf896fb3b9ea56aeff9b4c3cb87083f9e13d379
Uriel Mori <moriuriel@gmail.com>
2023-03-13 20:19:01 -0300

    test: criando novos testes

--- src/modules/users/infrastructure/repositories/InMemory/inMemoryUser.repository.ts
+++ src/modules/users/infrastructure/repositories/InMemory/inMemoryUser.repository.ts
@@ -1,13 +1,30 @@
-import { User } from '@Modules/users/domain/entites/user.entity';
+import { uuid } from 'uuidv4';
 import { IUserRepository } from '@Modules/users/domain/repositories/user.repository';
+import {
+  UnmarshalledUser,
+  User,
+} from '@Modules/users/domain/entites/user.entity';
 
 export class InMemoryUserRepository implements IUserRepository {
   private users: User[] = [];
 
   async create(user: User): Promise<User> {
-    this.users.push(user);
+    const unmarshalledUser: UnmarshalledUser = {
+      email: user.email,
+      name: user.name,
+      password: user.password,
+      isActive: false,
+      isConfirmed: false,
+      id: uuid(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
 
-    return user;
+    const newUser = new User(unmarshalledUser);
+
+    this.users.push(newUser);
+
+    return newUser;
   }
 
   async findByEmail(email: string): Promise<User> {
