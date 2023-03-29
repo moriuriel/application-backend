a77cd1279ce717fd31a67b46edacbfd47541fac3
Uriel Mori <moriuriel@gmail.com>
2023-03-28 22:09:28 -0300

    feat: ativando cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.enableVersioning({ type: VersioningType.URI });
 
   app.setGlobalPrefix('/api');
